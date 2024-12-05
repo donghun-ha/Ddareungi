95cce86851e5326371f9b3e1c8b509998cda318f
하동훈 <harukax99@naver.com>
2024-12-05 14:43:18 +0900

    sever 오타 => server

---
+++ python/server/router.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+import pymysql
+import hosts
+from fastapi.middleware.cors import CORSMiddleware
+from  songpa_office import router as test_router
+from jamsil.jamsil import router as jamsil_router
+
+
+app = FastAPI()
+
+
+app.include_router(test_router, prefix="/test", tags=["test"])
+app.include_router(jamsil_router, prefix="/jamsil", tags=["jamsil"])
+
+
+def connect():
+    conn = pymysql.connect(
+        host=hosts.ip, user="root", password="qwer1234", db="parking", charset="utf8"
+    )
+    return conn
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ['*'], # 모든 도메인 허용 : 테스트 할 때만 사용하기
+    allow_credentials = True,
+    allow_methods=['*'], # 모든 http 메서드 허용
+    allow_headers = ['*'], # 모든 헤더 사용
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
